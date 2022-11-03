09b2785356c4f51ce73cfccae65f29f525c50560
abdelkder_komti <akomti@anypli.com>
2022-11-03 16:53:02 +0100

    tables migrations

---
+++ database/migrations/2022_11_03_090156_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->tinyInteger('status')->default(0);
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('product_owner_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('product_owner_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
