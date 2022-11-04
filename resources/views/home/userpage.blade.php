d655604302d574e3422cdf35ebe558f860f830e6
abdelkder_komti <akomti@anypli.com>
2022-11-04 10:58:45 +0100

    building assets with vit

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -2,24 +2,31 @@
 <html>
 <head>
     <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <!-- Mobile Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <link rel="shortcut icon" href="/images/favicon.png" type="">
     <title>Famms - Fashion HTML Template</title>
     <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="/home/css/bootstrap.css" />
-    <!-- font awesome style -->
-    <link href="/home/css/font-awesome.min.css" rel="stylesheet" />
-    <!-- Custom styles for this template -->
-    <link href="/home/css/style.css" rel="stylesheet" />
-    <!-- responsive style -->
-    <link href="/home/css/responsive.css" rel="stylesheet" />
+    @vite([
+                'resources/home/css/bootstrap.css',
+                'resources/home/css/font-awesome.min.css',
+                'resources/home/css/responsive.css',
+                'resources/home/css/style.css',
+            ])
+    {{--    <link rel="stylesheet" type="text/css" href="{{asset('home/css/bootstrap.css')}}" />--}}
+    {{--    <!-- font awesome style -->--}}
+    {{--    <link href="{{asset('home/css/font-awesome.min.css')}}" rel="stylesheet" />--}}
+    {{--    <!-- Custom styles for this template -->--}}
+    {{--    <link href="{{asset('home/css/style.css')}}" rel="stylesheet" />--}}
+    {{--    <!-- responsive style -->--}}
+    {{--    <link href="{{asset('home/css/responsive.css')}}" rel="stylesheet" />--}}
+
 </head>
 <body>
 <div class="hero_area">
@@ -31,11 +38,11 @@
     <!-- end slider section -->
 </div>
 <!-- why section -->
-    @include('home.why')
+@include('home.why')
 <!-- end why section -->
 
 <!-- arrival section -->
-    @include('home.new_arrival')
+@include('home.new_arrival')
 <!-- end arrival section -->
 
 <!-- product section -->
@@ -58,12 +65,18 @@
     </p>
 </div>
 <!-- jQery -->
-<script src="/home/js/jquery-3.4.1.min.js"></script>
-<!-- popper js -->
-<script src="/home/js/popper.min.js"></script>
-<!-- bootstrap js -->
-<script src="/home/js/bootstrap.js"></script>
-<!-- custom js -->
-<script src="/home/js/custom.js"></script>
+@vite([
+            'resources/home/js/bootstrap.js',
+            'resources/home/js/custom.js',
+            'resources/home/js/jquery-3.4.1.min.js',
+            'resources/home/js/popper.min.js',
+        ])
+<script src="{{asset('home/js/jquery-3.4.1.min.js')}}"></script>
+{{--<!-- popper js -->--}}
+{{--<script src="{{asset('home/js/popper.min.js')}}"></script>--}}
+{{--<!-- bootstrap js -->--}}
+{{--<script src="{{asset('home/js/bootstrap.js')}}"></script>--}}
+{{--<!-- custom js -->--}}
+{{--<script src="{{asset('home/js/custom.js')}}"></script>--}}
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,21 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/home/css/bootstrap.css',
+                'resources/home/css/font-awesome.min.css',
+                'resources/home/css/responsive.css',
+                'resources/home/css/style.scss',
+
+
+                'resources/home/js/bootstrap.js',
+                'resources/home/js/custom.js',
+                'resources/home/js/jquery-3.4.1.min.js',
+                'resources/home/js/popper.min.js',
+            ],
             refresh: true,
         }),
     ],
+
 });
+
